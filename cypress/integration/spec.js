888f5da086e49e04b0262774ea0d8ede5e673e52
Rane Fields <rane.fields@eyecuelab.com>
2019-11-21 20:05:06 -0800

    Add test case

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,18 @@
-describe('page', () => {
-  it('works', () => {})
+describe('test case', () => {
+  it('works', () => {
+    cy.visit('/');
+    cy.get('#parent-element')
+      .as('test-alias');
+    cy.get('#delete-button').click();
+    cy.get('@test-alias').should('not.exist');
+  });
+
+  it('breaks', () => {
+    cy.visit('/');
+    cy.get('#child-element')
+      .parents('#parent-element')
+      .as('test-alias');
+    cy.get('#delete-button').click();
+    cy.get('@test-alias').should('not.exist');
+  });
 })
